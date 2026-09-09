5176642a0044260b0ffa095d5248b0021a8edb42
RenSafary <102967188+RenSafary@users.noreply.github.com>
2026-09-09 23:31:16 +0500

    checking cookie

--- routers/main_page.go
+++ routers/main_page.go
@@ -1,6 +1,7 @@
 package routers
 
 import (
+	"errors"
 	"html/template"
 	"log"
 	"net/http"
@@ -11,9 +12,19 @@ func MainPage(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Methods", "POST")
 	w.Header().Set("Acces-Control-Allow-Headers", "Content-Type")
 
-	tmpl, err := template.ParseFiles("./templates/main.html")
+	tmpl, err := template.ParseFiles("./templates/main_page.html")
 	if err != nil {
 		log.Println("Couldn't parse main_page.html:", err)
+		return
+	}
+
+	_, err = r.Cookie("session_token") // _ instead of 'cookie'
+	if err != nil {
+		if errors.Is(err, http.ErrNoCookie) {
+			http.Redirect(w, r, "/sign-in", http.StatusSeeOther)
+			return
+		}
+		http.Error(w, "Server error", http.StatusInternalServerError)
 		return
 	}
 
