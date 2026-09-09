d51bf001c8c54f1776952ceefc73f4c87bbb25c7
RenSafary <102967188+RenSafary@users.noreply.github.com>
2026-09-09 23:56:15 +0500

    parse data from form

--- routers/auth/sign_in.go
+++ routers/auth/sign_in.go
@@ -1,7 +1,20 @@
 package routers
 
-func SignIn() {
+import (
+	"database/sql"
+	"log"
+	"net/http"
+)
 
+func SignIn(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		log.Println("Could not parse 'sign-in' form")
+		return
+	}
+
+	username := r.FormValue("username")
+	password := r.FormValue("password")
 }
 
 func SignUp() {
