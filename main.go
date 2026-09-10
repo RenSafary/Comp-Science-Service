4e594643fc371c62dd936d001a3e55b47830986c
RenSafary <102967188+RenSafary@users.noreply.github.com>
2026-09-10 18:06:29 +0500

    db conn

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"Comp-Science-Service/database"
 	"Comp-Science-Service/routers"
+	"Comp-Science-Service/routers/auth"
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -11,11 +14,22 @@ import (
 func main() {
 	r := mux.NewRouter()
 
+	db, err := database.Conn()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	defer db.DB.Close()
+
+	h := &auth.AuthConf{
+		DB: db,
+	}
+
 	static := http.FileServer(http.Dir("./static"))
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", static))
 
 	r.HandleFunc("/", routers.MainPage).Methods("GET")
-	r.HandleFunc("/sign_in", routers.SignIn).Methods("POST", "GET")
+	r.HandleFunc("/sign_in", h.SignIn).Methods("POST")
 
 	ip := "http://127.0.0.1"
 	port := ":8080"
