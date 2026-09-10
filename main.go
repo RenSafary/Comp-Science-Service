080d677b9b01adafed96d130143aa112a2d9a065
RenSafary <102967188+RenSafary@users.noreply.github.com>
2026-09-10 19:05:33 +0500

    ws:sign-in

--- main.go
+++ main.go
@@ -29,7 +29,8 @@ func main() {
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", static))
 
 	r.HandleFunc("/", routers.MainPage).Methods("GET")
-	r.HandleFunc("/sign_in", h.SignIn).Methods("POST")
+	r.HandleFunc("/sign_in", h.SignInPage).Methods("GET")
+	r.HandleFunc("/sign_in_ws", h.SignIn)
 
 	ip := "http://127.0.0.1"
 	port := ":8080"
