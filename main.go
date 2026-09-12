b2c37cc96d62e74d51c21fa48529e0cc70bb8777
RenSafary <102967188+RenSafary@users.noreply.github.com>
2026-09-12 13:11:39 +0500

    sign up handlers

--- main.go
+++ main.go
@@ -29,8 +29,13 @@ func main() {
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", static))
 
 	r.HandleFunc("/", routers.MainPage).Methods("GET")
+
+	// authentication
 	r.HandleFunc("/sign_in", h.SignInPage).Methods("GET")
 	r.HandleFunc("/sign_in_ws", h.SignIn)
+
+	r.HandleFunc("/sign_up", h.SignInPage).Methods("GET")
+	r.HandleFunc("/sign_up_ws", h.SignIn)
 
 	ip := "http://127.0.0.1"
 	port := ":8080"
